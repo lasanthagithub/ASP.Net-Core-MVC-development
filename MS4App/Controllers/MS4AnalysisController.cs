59835623a6f1927b384781322d3774fb61010037
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-11-17 16:24:46 -0500

    MS4 Analysis mail view, controller and reuire logging autorization

---
+++ MS4App/Controllers/MS4AnalysisController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MS4App.Controllers
+{
+    public class MS4AnalysisController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
