6eaea01fa4c734842ed0b94666e135f54defb169
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-11-17 12:07:41 -0500

    Starting the project with the authentification

---
+++ MS4App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MS4App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
